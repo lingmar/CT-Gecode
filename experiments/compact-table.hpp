3399f3eb1d0ea60e78e4f088d58e77628a23744b
Linnea Ingmar <linnea.ingmar@hotmail.com>
2017-02-09 14:16:06 +0100

    Update header file and Makefile and add example files

--- experiments/compact-table.hpp
+++ experiments/compact-table.hpp
@@ -1,6 +1,11 @@
+#ifndef __COMPACT_TABLE__
+#define __COMPACT_TABLE__
+
 #include <gecode/int.hh>
 
 using namespace Gecode;
 using namespace Gecode::Int;
 
 void extensional2(Home home, const IntVarArgs& x, const TupleSet& t);
+
+#endif
