82aaab4b1d7f0443afa81d79afbbb22ab16bf72a
Linnea Ingmar <linnea.ingmar@hotmail.com>
2017-02-24 18:16:46 +0100

    Write benchmark script

--- benchmarks/crosswords/run.sh
+++ benchmarks/crosswords/run.sh
@@ -1,15 +1,47 @@
 #!/bin/bash
 
-export TABLE_PROPAGATOR="compact_table"
+#./run_gecode.sh
+#./run_compact_table.sh
 
-i=0
-while [ $i -lt 72 ]
+# Compare solutions, assuming delimiter "-" between solution print and statistics
+for i in {0..20}
 do
-    file="crossword3_$i.mzn"
-    mzn-gecode -s $file > "solutions/compact_table$i.txt"
-    i=$(($i + 1))
+    # Solution files
+    solfile_g="solutions/gecode_$i.txt"
+    solfile_c="solutions/compact_table$i.txt"
+    
+    solution_g=$(cat $solfile_g | cut -d% -f1)
+    solution_c=$(cat $solfile_c | cut -d% -f1)
+
+    runtime_g=$(cat $solfile_g | grep "runtime" | grep -o "[0-9]*.[0-9][0-9][0-9]" | head -n 1)
+    runtime_c=$(cat $solfile_c | grep "runtime" | grep -o "[0-9]*.[0-9][0-9][0-9]" | head -n 1)
+    
+    fail_g=$(cat $solfile_g | grep "failures" | grep -Eo "[0-9]{1,}")
+    fail_c=$(cat $solfile_c | grep "failures" | grep -Eo "[0-9]{1,}")
+
+    nprops_g=$(cat $solfile_g | grep "propagations" | grep -Eo "[0-9]{1,}")
+    nprops_c=$(cat $solfile_c | grep "propagations" | grep -Eo "[0-9]{1,}")
+
+    nsols_g=$(cat $solfile_g | grep "solutions" | grep -Eo "[0-9]{1,}")
+    nsols_c=$(cat $solfile_c | grep "solutions" | grep -Eo "[0-9]{1,}")
+    
+    if [ "$solution_c" != "$solution_g" ]; then
+        echo "[WARN]: solutions differ for instance $i"
+        diff <(echo $solution_g) <(echo $solution_c)
+    fi
+    if [ "$fail_g" != "$fail_c" ]; then
+        echo "[WARN]: number of failures differ for instance $i"
+        diff <(echo $fail_g) <(echo $fail_c)
+    fi
+    # if [ $nprops_g != $nprops_c ]; then
+    #     echo "[WARN]: number of propagations differ for instance $i"
+    #     diff <(echo $nprops_g) <(echo $nprops_c)
+    # fi
+    if [ "$nsols_g" != "$nsols_c" ]; then
+        echo "[WARN]: number of solutions differ for instance $i"
+        diff <(echo $nsols_g) <(echo $nsols_c)
+    fi
+
+    echo "$i & $runtime_g & $fail_g & $runtime_c & $fail_c \\\\"
+    
 done
-
-
-
-
