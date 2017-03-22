30f5bf294c66e438c8c0abd47f7086ef7d1be40d
Linnea Ingmar <liin3244@siegbahn.it.uu.se>
2017-03-22 18:17:25 +0100

    Add script for compact-table + run benchmarks for i = 1..9

---
+++ benchmarks/crosswords/run_compact-table.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export TABLE_PROPAGATOR="compact-table"
+
+i=0
+while [ $i -lt 10 ]
+do
+    file="mzn/crossword3_$i.mzn"
+    ${MZN_PATH}/mzn-fzn -f ${GECODE_PATH}/bin/fzn-gecode -I ${GECODE_PATH}/share/gecode/mznlib -Ggecode -s $file > "solutions/compact-table_$i.txt"
+    #fzn-gecode -s $file > "solutions/gecode_$i.txt"
+    i=$(($i + 1))
+done
