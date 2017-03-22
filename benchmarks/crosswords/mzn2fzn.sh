afd9be478797046a337a3440667b4fface0eee90
Linnea Ingmar <linnea.ingmar@hotmail.com>
2017-03-22 16:01:29 +0100

    Add -Ggecode flag to script and add fzn-files

--- benchmarks/crosswords/mzn2fzn.sh
+++ benchmarks/crosswords/mzn2fzn.sh
@@ -6,6 +6,6 @@ i=0
 while [ $i -lt 72 ]
 do
     file="mzn/crossword3_$i.mzn"
-    mzn2fzn $file > "fzn/crossword3_${i}_$TABLE_PROPAGATOR.fzn"
+    mzn2fzn -Ggecode -o "fzn/crossword3_${i}_$TABLE_PROPAGATOR.fzn" $file
     i=$(($i + 1))
 done
