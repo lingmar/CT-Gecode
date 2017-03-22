edd29d6c1199c80da65cff25bba280da984df2d6
Linnea Ingmar <linnea.ingmar@hotmail.com>
2017-03-22 18:49:20 +0100

    Change limit to 72

--- benchmarks/crosswords/run_compact-table.sh
+++ benchmarks/crosswords/run_compact-table.sh
@@ -3,7 +3,7 @@
 export TABLE_PROPAGATOR="compact-table"
 
 i=0
-while [ $i -lt 10 ]
+while [ $i -lt 72 ]
 do
     file="mzn/crossword3_$i.mzn"
     ${MZN_PATH}/mzn-fzn -f ${GECODE_PATH}/bin/fzn-gecode -I ${GECODE_PATH}/share/gecode/mznlib -Ggecode -s $file > "solutions/compact-table_$i.txt"
