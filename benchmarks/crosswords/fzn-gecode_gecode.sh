466415750b94368d2db04c0fdde07d91fc07f36f
Linnea Ingmar <linnea.ingmar@hotmail.com>
2017-03-23 17:32:55 +0100

    fzn-gecode scripts

---
+++ benchmarks/crosswords/fzn-gecode_gecode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export TABLE_PROPAGATOR="gecode"
+
+i=22
+while [ $i -lt 72 ]
+do
+    file="fzn/crossword3_${i}_gecode.fzn"
+    ${GECODE_PATH}/bin/fzn-gecode -s $file > "solutions/gecode_$i.txt"
+    #fzn-gecode -s $file > "solutions/gecode_$i.txt"
+    i=$(($i + 1))
+done
