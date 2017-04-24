42c9b2467a3ecc071127cc0fc9d4be77694968ef
Linnea Ingmar <linnea.ingmar@hotmail.com>
2017-04-24 13:06:19 +0200

    Add ozn files and benchmarks data

--- benchmarks/or-tools/process.sh
+++ benchmarks/or-tools/process.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-find . -name "*.res" | parallel -P 4 --progress nice ./processone.sh {}
+#find . -name "*.res" | parallel -P 4 --progress nice ./processone.sh {}
 
+REG="reg.log"
+TUP="tup.log"
+CT="ct.log"
 
+touch $REG
+touch $TUP
+touch $CT
 
+cat out.log | grep "reg" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n > $REG
+cat out.log | grep "tup" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n > $TUP
+cat out.log | grep "ct" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n > $CT
 
-
-
-
+for f in $REG $TUP $CT; do
+    count=1
+    for line in `cat $f`; do
+        echo "$line $count" >> "$(echo $f | cut -d '.' -f1).data"
+        let "count+=1"
+    done
+done
--- benchmarks/or-tools/processone.sh
+++ benchmarks/or-tools/processone.sh
@@ -9,13 +9,36 @@ touch $ERR
 OUT="out.log"
 touch $OUT
 
-grep -q "COMPACT" $T &&
-    grep -q "REGULAR" $T &&
-    grep -q "TUPLESET" $T ||
-        {
-            echo "$1" >> err.log
-            exit 2
-        }
+missing=""
+
+grep -q "REGULAR" $T ||
+    {
+        missing="$missing reg"
+    }
+grep -q "TUPLESET" $T ||
+    {
+        missing="$missing tup"
+    }
+
+grep -q "COMPACT" $T ||
+    {
+        missing="$missing ct"
+    }
+
+#do
+if [ "$missing" != "" ]
+then
+    echo "Missing $missing: $T" >> err.log
+    exit 2
+fi
+
+nsolved=$(grep "solvetime" $T | wc -l | grep -o "[0-9]")
+
+if [ $nsolved != 3 ]
+then
+    echo "Solved $nsolved: $T" >> err.log
+    exit 2
+fi
 
 prop[0]="reg"
 prop[1]="tup"
@@ -23,9 +46,14 @@ prop[2]="ct"
 
 count=0
 
-echo $T >> $OUT
+#echo $T >> $OUT
 
 for time in `grep "solvetime" $T | grep -o "([0-9]*.[0-9][0-9][0-9]" | cut -d "(" -f2`; do
     echo "${prop[$count]}($time)" >> $OUT
     let count=$count+1
 done
+
+
+
+
+
