2dcd62632404839006694b78e9616c0b5c1c127a
Linnea Ingmar <linnea.ingmar@hotmail.com>
2017-05-08 21:13:24 +0200

    Set memory limit in scriptrun

--- benchmarks/or-tools/scriptrun.sh
+++ benchmarks/or-tools/scriptrun.sh
@@ -2,8 +2,9 @@
 
 # Run fzn-gecode for many files
 
-#find . -name "*.fzn" | tail -n +$1 | head -n +$2 | parallel --gnu -P 4 --load=50% --progress nice  ./scriptrunone.sh {}
-cat rerun.txt | tail -n +$1 | head -n +$2 | parallel --gnu -P 1 --load=50% --progress nice  ./scriptrunone.sh {}
+find $1 -name "*.fzn" | parallel --gnu -P 4 --load=50% --progress --noswap --memfree 4G nice ./scriptrunone.sh {}
+#find . -name "*.fzn" | tail -n +$1 | head -n +$2 | parallel --gnu -P 4 --load=50% --progress --noswap --memfree 4G nice ./scriptrunone.sh {}
+#cat rerun.txt | tail -n +$1 | head -n +$2 | parallel --gnu -P 1 --load=50% --progress nice  ./scriptrunone.sh {}
 
 #./process.sh $1 $2
 
