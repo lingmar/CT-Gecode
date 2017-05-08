d89c274384a8b12e24b9212e0c3095fd42d657bf
Linnea Ingmar <liin3244@siegbahn.it.uu.se>
2017-05-08 21:07:04 +0200

    Add scripts

--- benchmarks/or-tools/scriptrun.sh
+++ benchmarks/or-tools/scriptrun.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # Run fzn-gecode for many files
 
-find . -name "*.fzn.lzma" | tail -n +$1 | head -n +$2 | parallel --gnu -P 16 --progress nice  ./scriptrunone.sh {}
+#find . -name "*.fzn" | tail -n +$1 | head -n +$2 | parallel --gnu -P 4 --load=50% --progress nice  ./scriptrunone.sh {}
+cat rerun.txt | tail -n +$1 | head -n +$2 | parallel --gnu -P 1 --load=50% --progress nice  ./scriptrunone.sh {}
 
-./process.sh $1 $2
+#./process.sh $1 $2
 
 
 
--- benchmarks/or-tools/scriptrunone.sh
+++ benchmarks/or-tools/scriptrunone.sh
@@ -1,16 +1,21 @@
+#!/bin/bash
 # Script for running fzn-gecode
-T=$1
-unlzma $T
 
-FZN=${T%.*}
+echo "hej"
+FZN=$1
+#unlzma $T
+
+#FZN=${T%.*}
 
 OUT=$(echo $FZN | rev | cut -f 2- -d '.' | rev).res
+touch $OUT
+rm $OUT
 touch $OUT
 
 for prop in "gecode-regular" "gecode-tupleset-mem" "gecode-tupleset-speed" "compact-table"; do
     export TABLE_PROPAGATOR=$prop
-    ${GECODE_PATH}/bin/fzn-gecode -time 1 -s $FZN >> $OUT
+    ${GECODE_PATH}/bin/fzn-gecode -time 1000000 -s $FZN >> $OUT
 done
 
 # Compress fzn file 
-lzma $FZN 
+#lzma $FZN
