7a7f0eabad6ab93520af015df8fa0281712b11b6
Linnea Ingmar <linnea.ingmar@hotmail.com>
2017-05-09 10:36:28 +0200

    Update process.sh to handle one directory at a time

--- benchmarks/or-tools/process.sh
+++ benchmarks/or-tools/process.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#rm `find . -name "*.log"`
-#rm `find . -name "*.data"`
+dir=./instances/$1
+echo $dir
+
+rm `find . -name "*.log"`
+rm `find . -name "*.data"`
 
 #find . -name "*.res" | tail -n +$1 | head -n +$2 | parallel -P 16 --progress nice ./processone.sh {}
+find $dir -name "*.res" parallel -P 4 --progress nice ./processone.sh {}
 
 echo "Done processone"
 
@@ -15,41 +19,35 @@ CT="ct.log"
 RAW="out.log"
 OUT=$(echo $f | cut -d '.' -f1).data
 
-#for dir in "./instances/randsJC2500"; do
-for dir in `ls instances`; do
-    dir=instances/$dir
-    echo $dir
-    #touch $dir/$REG
-    #touch $dir/$TUP_MEM
-    #touch $dir/$TUP_SPEED
-    #touch $dir/$CT
+touch $dir/$REG
+touch $dir/$TUP_MEM
+touch $dir/$TUP_SPEED
+touch $dir/$CT
 
-    #rm $dir/$REG
-    #rm $dir/$TUP_MEM
-    #rm $dir/$TUP_SPEED
-    #rm $dir/$CT
+rm $dir/$REG
+rm $dir/$TUP_MEM
+rm $dir/$TUP_SPEED
+rm $dir/$CT
 
-    #cat $dir/out.log | grep "reg" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n >$dir/$REG
-    #cat $dir/out.log | grep "tup_mem" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n > $dir/$TUP_MEM
-    #cat $dir/out.log | grep "tup_speed" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n > $dir/$TUP_SPEED
-    #cat $dir/out.log | grep "ct" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n > $dir/$CT
+cat $dir/out.log | grep "reg" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n >$dir/$REG
+cat $dir/out.log | grep "tup_mem" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n > $dir/$TUP_MEM
+cat $dir/out.log | grep "tup_speed" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n > $dir/$TUP_SPEED
+cat $dir/out.log | grep "ct" | cut -d "(" -f2 | cut -d ")" -f1 | sort -n > $dir/$CT
 
-    prop=(reg tup_mem tup_speed ct)
-    index=0
+prop=(reg tup_mem tup_speed ct)
+index=0
 
-    for f in $dir/$REG $dir/$TUP_MEM $dir/$TUP_SPEED $dir/$CT; do
-        count=1
-	OUT="${prop[index]}.data"
-	rm $dir/$OUT
-        for line in `cat $f`; do
-            echo "($line, $count)" >> "$dir/$OUT"
- #           echo "$line $count -> $dir/$OUT"
-	    count=$(($count+1))
-        done
-	index=$(($index+1))
+for f in $dir/$REG $dir/$TUP_MEM $dir/$TUP_SPEED $dir/$CT; do
+    count=1
+    OUT="${prop[index]}.data"
+    rm $dir/$OUT
+    for line in `cat $f`; do
+        echo "($line, $count)" >> "$dir/$OUT"
+        #           echo "$line $count -> $dir/$OUT"
+	count=$(($count+1))
     done
-
+    index=$(($index+1))
 done
 
-dirname `find . -name "ct.data"` | parallel --gnu -P 8 --progress nice ./generate-graph.sh {}
+dirname `find $dir -name "ct.data"` | parallel --gnu -P 8 --progress nice ./generate-graph.sh {}
 
