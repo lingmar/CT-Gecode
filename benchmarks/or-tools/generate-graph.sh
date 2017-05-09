78549ee1187c316fd0407ce49aeb7f4401852756
Linnea Ingmar <linnea.ingmar@hotmail.com>
2017-05-09 10:52:08 +0200

    Update generate-graph.sh

--- benchmarks/or-tools/generate-graph.sh
+++ benchmarks/or-tools/generate-graph.sh
@@ -9,15 +9,8 @@ CT=$T/ct.data
 
 GRAPH=$ct/report/$1.tex
 
-if [ -d `dirname $GRAPH` ]; then
-    # ok
-else 
-    mkdir `dirname $GRAPH`
-fi
 
-
-echo "\begin{tikzpicture}[scale=1.0]
-  \begin{axis}[
+echo "\begin{axis}[
     xmode=log,
     ymin=0,ymax=`cat ct-ot.data | wc -l`,
     xmin=0.1, xmax=1000000,
@@ -34,26 +27,33 @@ echo "\begin{tikzpicture}[scale=1.0]
     mark size=1.5,
     mark options={solid},
     green] 
-    table {$REG};
+    coordinates {
+    $(cat $REG)
+    };
 
     \addplot 
     [blue,
     mark=*,
     mark size=1.5,
     mark options={solid}]
-    table {$TUP_MEM};
+    coordinates {
+    $(cat $TUP_MEM)
+    };
 
     \addplot [brown!60!black,
     mark options={fill=brown!40},
     mark=otimes*,
     mark size=1.5]
-    table {$TUP_SPEED};
+    coordinates {
+    $(cat $TUP_SPEED)
+    };
 
     \addplot 
     [red,
     mark size=1.5,
     mark=square*]
-    table {$CT};
-    \legend{Reg,Tup\_mem,Tup\_speed,CT}
-  \end{axis}
-\end{tikzpicture}" > or-tools-plot.tex
+    coordinates {
+    $(cat $CT)
+    };
+    \legend{DFA,B,I,CT}
+  \end{axis}" > or-tools-plot.tex
